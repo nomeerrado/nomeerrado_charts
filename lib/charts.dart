320ea56bbd2576ae0a07bb1d338bf884f47135fb
Gean <r3k4hg4m3r@gmail.com>
2022-01-26 09:45:27 -0300

    exports

--- lib/charts.dart
+++ lib/charts.dart
@@ -1,2 +1,7 @@
 export 'bar_chart/bar_chart.dart';
 export 'pie_chart/pie_chart.dart';
+
+export 'pie_chart/core/chart_values_options.dart';
+export 'pie_chart/core/degree_options.dart';
+export 'pie_chart/core/legend_options.dart';
+export 'pie_chart/core/utils.dart';
